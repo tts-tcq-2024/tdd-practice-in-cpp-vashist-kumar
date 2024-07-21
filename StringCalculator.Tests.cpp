c80b2764397bd89cdeb52698571b4ee93a310807
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-21 18:30:28 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -28,13 +28,13 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
-//     ASSERT_THROW({
-//         std::string input = "-1,2";
-//         StringCalculator objUnderTest;
-//        objUnderTest.add(input);
-//         }, std::runtime_error);
-// }
+TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
+    ASSERT_THROW({
+        std::string input = "-1,2";
+        StringCalculator objUnderTest;
+       objUnderTest.add(input);
+        }, std::runtime_error);
+}
 
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
@@ -57,6 +57,24 @@ TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
     std::string input = "//;\n1;2";
+    StringCalculator objUnderTest;
+    int result = objUnderTest.add(input);
+
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int expectedresult = 3;
+    std::string input = "//[***]\n1***2***3";
+    StringCalculator objUnderTest;
+    int result = objUnderTest.add(input);
+
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int expectedresult = 3;
+    std::string input = "1,2,3";
     StringCalculator objUnderTest;
     int result = objUnderTest.add(input);
 
